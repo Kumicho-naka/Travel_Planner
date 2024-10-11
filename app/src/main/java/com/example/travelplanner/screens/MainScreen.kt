eba740652f2acb42dda1de8f1ce2714f4ad61b69
DESKTOP-U4V040V\Nakano <masamitsun@array.co.jp>
2024-10-11 16:42:09 +0900

    初回追加

---
+++ app/src/main/java/com/example/travelplanner/screens/MainScreen.kt
@@ -0,0 +1,82 @@
+package com.example.travelplanner.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.travelplanner.R
+
+@Composable
+fun MainScreen(){
+    val image = painterResource(R.drawable.free_item_main_background)
+    Box(modifier = Modifier.fillMaxSize()){
+        Image(painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "旅行日程管理",
+                style = MaterialTheme.typography.displayLarge,
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(64.dp)) {
+                Text(text = "プラン作成",
+                    style =  MaterialTheme.typography.headlineMedium
+                )
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(64.dp)) {
+                Text("プラン確認",
+                    style =  MaterialTheme.typography.headlineMedium
+                )
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(64.dp)) {
+                Text("プラン削除",
+                    style =  MaterialTheme.typography.headlineMedium
+                )
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview(){
+        MainScreen()
+}
